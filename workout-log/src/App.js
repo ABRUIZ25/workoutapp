1eba1d827562829856bcba94a406c0dec11b3dbb
ABRUIZ25 <89878760+ABRUIZ25@users.noreply.github.com>
2022-03-21 11:54:14 -0400

    layout/state has been made

--- workout-log/src/App.js
+++ workout-log/src/App.js
@@ -1,9 +1,57 @@
 import React, { Component } from 'react'
 
 export class App extends Component {
+  state = {
+    workout: [
+      {
+        name: "",
+        oneMax: "",
+        50: "",
+        75: "",
+        80: "",
+        90: "",
+      }
+    ]
+  }
+
+
+
   render() {
     return (
       <div>
+
+        <h1>
+          Welcome your workout log
+        </h1>
+
+        <div>
+          <h3>
+            Enter your workout here
+          </h3>
+          <input>
+          </input>
+          <h4>
+            Enter your one max weight
+          </h4>
+          <input>
+          </input>
+          <div>
+            <button>
+              Log
+            </button>
+          </div>
+        </div>
+
+        <h3>
+          Your workouts
+        </h3>
+        <div className='new weight'>
+          50%:
+          75%:
+          80%:
+          90%:
+        </div>
+
 
 
       </div>
